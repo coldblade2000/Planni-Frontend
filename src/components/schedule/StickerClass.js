5a8ab84370e321bfb27f8134e0bc21158d02b694
Veronica Escobar <vescobars2001@gmail.com>
2021-01-14 20:22:04 -0500

    Implemented abreviation of the teachers name in the stickers that are on the schedule now instead of displaying the full name it displays the teachers first name initials and its last name 'Anamaria Mojica Hanke' would show as A.Mojica

--- src/components/schedule/StickerClass.js
+++ src/components/schedule/StickerClass.js
@@ -15,12 +15,32 @@ export class StickerClass {
 const findPrimaryFaculty = (course)=>{
     for (const professor of course['faculty']){
         if(professor['isFaculty']===true){
-            return professor.displayName
+            //return professor.displayName
+           
+            var prof = professor.displayName.split(",");
+            var fName = prof[1].toLowerCase(); //First Letter of First Name
+            fName = fName.charAt(1).toUpperCase();
+            var lName = prof[0].split(" ")[0];//First last Name
+            var profAbreviation = fName + "." + lName;
+            return profAbreviation
         }
     }
     if (course['faculty'].length===0){
         return null
     }else{
-        return course['faculty'][0].displayName
+        //return course['faculty'][0].displayName
+        
+        var prof = course['faculty'][0].displayName.split(",");
+        var fName = prof[1].toLowerCase(); //First Letter of First Name
+        fName = fName.charAt(1).toUpperCase();
+        var lName = prof[0].split(" ");//First last Name
+        if(lName.length == 3){
+            lName = lName[0] + " " + lName[1];
+        }
+        else{
+            lName = lName[0];
+        }
+        var profAbreviation = fName + "." + lName;
+        return profAbreviation
     }
 }
