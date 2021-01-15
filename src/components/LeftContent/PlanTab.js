ee72d9cc36d0ec94a5b443ac615c565ee221f39f
coldblade2000 <diegogranada7@gmail.com>
2021-01-14 23:59:37 -0500

    Added a counter for how many credits the plan has

--- src/components/LeftContent/PlanTab.js
+++ src/components/LeftContent/PlanTab.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import CourseList from "./CourseList";
+import {Divider, Typography} from "@material-ui/core";
+import '../stylesheets/PlanTab.css'
 
 const PlanTab = (props) => {
 
+    const calculateCredits = () => {
+        let num = 0
+        for (let course of props.plan.courseList) {
+            if (course.credits) num = num + course.credits
+        }
+        return num
+    }
+
     return (
-        <CourseList usesSearch={false}/>
+        <div className="plantab">
+            <div className="topInfo headerItem">
+                <Typography variant='h3'>{props.plan.name}</Typography>
+            </div>
+            <Divider/>
+            <Typography className="headerItem centertext" variant="subtitle1" gutterBottom>
+                {`You have selected ${calculateCredits()} credits`}
+            </Typography>
+            <CourseList usesSearch={false}/>
+        </div>
     )
 }
 
 
 function mapStateToProps(state) {
-    return {};
+    return {plan: state.selectedPlan};
 }
 
 export default connect(mapStateToProps, null)(PlanTab);
