0ac0ad2209220c3a53c498a5f5a3d0f3b4a8adb9
coldblade2000 <diegogranada7@gmail.com>
2021-07-29 00:58:59 -0500

    Fixed issue with sticker faculty name

--- src/components/schedule/StickerClass.js
+++ src/components/schedule/StickerClass.js
@@ -1,5 +1,4 @@
 export class StickerClass {
-
     constructor(course, meeting, color) {
         const subtitle = course.courseTitle + "\n" + findPrimaryFaculty(course)
         this.title = course.courseIdentifier;
@@ -14,46 +13,34 @@ export class StickerClass {
 }
 
 const findPrimaryFaculty = (course) => {
-    for (const professor of course['faculty']) {
-        if (professor['isFaculty'] === true) {
-            //return professor.displayName
-
-            let prof = professor.displayName.split(",");
-            let fName = prof[1].split(" ")
-            if (fName.length >= 2) {
-                fName = fName[0].charAt(1).toUpperCase() + fName[1].charAt(2).toLowerCase();
-            } else {
-                fName = fName[0].charAt(1).toUpperCase();
-            }
-            let lName = prof[0].split(" ");//First last Name
-            if (lName.length === 3) {
-                lName = lName[0] + " " + lName[1];
-            } else {
-                lName = lName[0];
-            }
-            return fName + "." + lName
-        }
-    }
     if (course['faculty'].length === 0) {
         return null
     } else {
-        //return course['faculty'][0].displayName
+        let prof = undefined;
+        for (const professor of course['faculty']) {
+            if (professor['isPrimary'] === true) {
+                prof = professor.displayName.split(",");
+            }
+        }
+        if (!prof) {
+            prof = course['faculty'][0].displayName.split(",");
+        }
+        const firstNames = prof[1].split(" ")
+        const lastNames = prof[0].split(" ")
 
-        let prof = course['faculty'][0].displayName.split(",");
-        let fName = prof[1].split(" ")
-        fName[0] = fName[0].trim()
-        if (fName.length >= 2) {
-            fName[1] = fName[1].trim();
-            fName = fName[0].charAt(0).toUpperCase() + fName[1].charAt(0).toUpperCase();
-        } else {
-            fName = fName[0].charAt(1).toUpperCase();
+        let firstNameInitials = ""
+        for (let firstName of firstNames) {
+            firstName = firstName.trim()
+            if (firstName.length > 0) {
+                firstNameInitials += firstName.toUpperCase().charAt(0) + "."
+            }
         }
-        let lName = prof[0].split(" ");//First last Name
-        if (lName.length === 3) {
-            lName = lName[0] + " " + lName[1];
-        } else {
-            lName = lName[0];
-        }
-        return fName + "." + lName
+        return titleCase(`${firstNameInitials} ${titleCase(lastNames[0])}`)
     }
+}
+
+function titleCase(str) {
+    return str.replace(/(^|\s)\S/g, function (t) {
+        return t.toUpperCase()
+    });
 }
